eb6c66277ab5c910d157cd0fc5883fded0633f69
Corey Ward <corey.atx@gmail.com>
2020-04-13 16:04:02 -0500

    Add JSdoc documentation

--- src/fields/slug.js
+++ src/fields/slug.js
@@ -1,19 +1,24 @@
 import urlJoin from "../lib/url-join.js"
 import slugify from "slugify"
 
-const createSlug = input =>
+/**
+ * Create a url-friendly slug with a limited character set.
+ *
+ * @param {string} input
+ */
+const createSlug = (input) =>
   slugify(input, { lower: true, remove: /[^a-zA-Z0-9 -]/g })
 
 const slug = {
   title: "Slug",
   name: "slug",
   type: "slug",
-  validation: Rule =>
+  validation: (Rule) =>
     Rule.custom(
       ({ current: slug } = {}) => validateFormat(slug) || "Invalid formatting"
     ),
   options: {
-    slugify: source => urlJoin("/", createSlug(source), "/"),
+    slugify: (source) => urlJoin("/", createSlug(source), "/"),
   },
   description:
     "The `slug` becomes the path of the published page on the website. It will be appended to the domain name automatically.",
@@ -21,6 +26,17 @@ const slug = {
 
 export default slug
 
+/**
+ * Generate a slug-type Sanity field with validation
+ *
+ * @param {Object} options
+ * @param {string} options.source - Name of the document field to use as a source for the slug generator
+ * @param {string} [options.prefix] - Require the slug to begin with a specific path
+ * @param {string} [options.name] - Name of the slug field
+ * @param {string} [options.title] - Title of the slug field
+ * @param {function} [options.slugify] - Slugging function
+ * @returns {Object} Sanity field definition
+ */
 export const createSlugField = ({ prefix, ...options }) => {
   prefix = urlJoin("/", prefix, "/")
 
@@ -32,7 +48,7 @@ export const createSlugField = ({ prefix, ...options }) => {
         const obj = opts.parentPath.reduce((doc, path) => {
           if (typeof path === "object") {
             const [[property, value]] = Object.entries(path)
-            return doc.find(d => d[property] === value)
+            return doc.find((d) => d[property] === value)
           } else {
             return doc[path]
           }
@@ -40,9 +56,9 @@ export const createSlugField = ({ prefix, ...options }) => {
 
         return obj[options.source]
       },
-      slugify: source => urlJoin(prefix, createSlug(source), "/"),
+      slugify: (source) => urlJoin(prefix, createSlug(source), "/"),
     },
-    validation: Rule =>
+    validation: (Rule) =>
       Rule.required().custom(({ current: value } = {}) => {
         if (!value) return "Slug is required"
         if (!validateFormat(value)) return "Invalid formatting"
@@ -54,7 +70,7 @@ export const createSlugField = ({ prefix, ...options }) => {
   }
 }
 
-const validateFormat = slug => {
+const validateFormat = (slug) => {
   if (!slug) return false
   switch (slug.length) {
     case 1:
