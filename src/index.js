87a3609a8e6c8f94f00812042ca71106fea2b545
Corey Ward <corey.atx@gmail.com>
2020-04-13 16:03:50 -0500

    Export field factories

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { default as imageField } from "./fields/image"
-export { default as slugField } from "./fields/slug"
+export { default as imageField, createImageField } from "./fields/image"
+export { default as slugField, createSlugField } from "./fields/slug"
 export { default as decodeAssetId } from "./lib/decode-asset-id"
 export { default as urlJoin } from "./lib/url-join"
