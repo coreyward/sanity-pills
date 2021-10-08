6926993633e09d22a48a68dc47ebd73b3c08de2a
Corey Ward <corey@figma.com>
2021-10-08 12:24:06 -0500

    Add noDuplicateRefs routine

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+// Fields
 export { default as imageField, createImageField } from "./fields/image"
 export { default as slugField, createSlugField } from "./fields/slug"
+
+// Block stuff
 export { default as blockPreview } from "./lib/blockPreview"
 export { createBlockField } from "./fields/block"
 export {
@@ -7,6 +10,9 @@ export {
   defaultBlockValidator,
   blockValidations,
 } from "./lib/blockValidator"
+
+// Utilities
 export { default as decodeAssetId } from "./lib/decodeAssetId"
 export { default as fields } from "./lib/fieldsHelper"
 export { default as urlJoin } from "./lib/urlJoin"
+export { default as noDuplicateRefs } from "./lib/noDuplicateRefs"
---
+++ src/lib/noDuplicateRefs.js
@@ -0,0 +1,31 @@
+/**
+ * Prevents the same document from being referenced multiple times in an array
+ * of references. Pass as the value to the `filter` option on the reference
+ * declaration itself (not the array).
+ *
+ * @example
+ * const field = {
+ *   type: "array",
+ *   of: [
+ *     {
+ *       type: "reference",
+ *       to: [{ type: "someDocumentType" }],
+ *       options: {
+ *         filter: noDuplicateRefs,
+ *       },
+ *     },
+ *   ],
+ * }
+ */
+export default ({ parent }) => {
+  const existingRefs = parent.map((item) => item._ref).filter(Boolean)
+
+  return existingRefs.length
+    ? {
+        filter: "!(_id in $ids)",
+        params: {
+          ids: existingRefs,
+        },
+      }
+    : true
+}
