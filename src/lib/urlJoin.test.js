91bd01aaa9721bb42e52a9f1e95652720da209c8
Corey Ward <corey@figma.com>
2021-12-16 10:01:35 -0600

    Add additional urlJoin tests

--- src/lib/urlJoin.test.js
+++ src/lib/urlJoin.test.js
@@ -24,8 +24,14 @@ test("internal delimiters ignored", () => {
   expect(urlJoin("/f/o/o/", "/b/a/r/")).toBe("/f/o/o/b/a/r/")
 })
 
+test("does not insert preceding and trailing slashes", () => {
+  expect(urlJoin("foo", "bar")).toBe("foo/bar")
+})
+
 test("single argument returned as-is", () => {
-  expect(urlJoin("hi")).toBe("hi")
+  expect(urlJoin("foo")).toBe("foo")
+  expect(urlJoin("foo/")).toBe("foo/")
+  expect(urlJoin("/foo")).toBe("/foo")
 })
 
 test("delimiters collapsed infinitely", () => {
